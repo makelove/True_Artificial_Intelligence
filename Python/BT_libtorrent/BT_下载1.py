1d55071a129e6ff13ef59e42239f6121bc40173a
play4fun <play4fun@foxmail.com>
2018-01-04 17:54:29 +0800

    bt complete

--- Python/BT_libtorrent/BT_下载1.py
+++ Python/BT_libtorrent/BT_下载1.py
@@ -23,7 +23,7 @@ import sys
 ses = lt.session()
 ses.listen_on(6881, 6891)
 
-info = lt.torrent_info(sys.argv[1])
+info = lt.torrent_info(sys.argv[1])#种子文件
 h = ses.add_torrent({'ti': info, 'save_path': './'})
 print 'starting', h.name()
 
@@ -40,3 +40,9 @@ while (not h.is_seed()):
    time.sleep(1)
 
 print h.name(), 'complete'
+
+'''
+(.py3) pro:test3 play$ python2 /Users/play/github/True_Artificial_Intelligence/Python/BT_libtorrent/BT_下载1.py  ./\[zooqle.com\]\ American\ Housewife\ S02E11\ HDTV\ x264-SVA\ \[eztv\].torrent
+starting American.Housewife.S02E11.HDTV.x264-SVA[eztv].mkv
+99.90% complete (down: 276.0 kb/s up: 151.0 kB/s peers: 80) downloading American.Housewife.S02E11.HDTV.x264-SVA[eztv].mkv complete
+'''
