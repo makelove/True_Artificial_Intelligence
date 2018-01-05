ef77c080abe07dd75c8cf1b898a4aec7afc0ea8a
play4fun <play4fun@foxmail.com>
2018-01-05 20:11:10 +0800

    加密

---
+++ Python/代码加密/helloworld.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/1/5 19:39
+# @Author  : play4fun
+# @File    : helloworld.py
+# @Software: PyCharm
+
+"""
+helloworld.py:
+"""
+
+hw='helloworld'
+
+print(hw)
