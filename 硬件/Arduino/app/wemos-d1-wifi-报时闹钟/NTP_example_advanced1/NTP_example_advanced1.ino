e708cc48dce3f68d3c8bb6da9ee99c32b497a009
play4fun <play4fun@foxmail.com>
2018-01-19 16:16:05 +0800

    报时-时间段2

--- 硬件/Arduino/app/wemos-d1-wifi-报时闹钟/NTP_example_advanced1/NTP_example_advanced1.ino
+++ 硬件/Arduino/app/wemos-d1-wifi-报时闹钟/NTP_example_advanced1/NTP_example_advanced1.ino
@@ -50,7 +50,7 @@ void loop() {
   //  minute=Seconds;
 
   Serial.println(timeClient.getFormattedTime());
-  for (int i = 8; i <= 22; i++) {
+  for (int i = 8; i <= 22; i++) {//时间段
     if (hour == i) {
       //
       if (minute == 0) {
